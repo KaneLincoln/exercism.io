baa0f7f9712ec33be2bbfe7b8e3dbea704c21fa0
Kane Lincoln <Kane.Lincoln@icloud.com>
2018-10-06 13:17:17 +0100

    updated solution based on mentor feedback

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,6 +1,7 @@
 export function isLeap(year) {
-  if (year % 100 !== 0) {
-    return year % 4 === 0 ? true : false;
-  }
-  return year % 400 === 0 ? true : false;
+  if (year % 4 === 0) {
+    return year % 100 !== 0 || (year % 100 === 0 && year % 400 === 0) ? true : false;
+  } else {
+    return false;
+  };
 }
